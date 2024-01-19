ab7c9e9edbcd8928b99b585190c806488daeb765
Leonardo Ávila <leonardo.avila98@outlook.com>
2024-01-19 15:39:06 -0300

    add terraform

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "lab_account_id" {
+  description = "AWS Labs account ID"
+}
+
+variable "lab_account_region" {
+  description = "AWS Labs account region"
+  default = "us-west-2"
+}
+
+variable "food_totem_demand_image" {
+  description = "Food Totem Demand Docker image"
+}
+
+variable "mongo_image" {
+  description = "Mongo Docker image"
+}
+
+variable "mongo_root_user" {
+  description = "Mongo root user"
+}
+
+variable "mongo_root_password" {
+  description = "Mongo root password"
+}
